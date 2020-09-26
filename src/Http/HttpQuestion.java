043f9a1f12265c78f78af0b9394d162f45a8db63
SerKKiT <serjtimakov@yandex.ru>
2020-09-26 21:41:41 +0300

    Update HttpQuestion class

--- src/Http/HttpQuestion.java
+++ src/Http/HttpQuestion.java
@@ -4,26 +4,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Обрабатывает и хранит HTTP запрос, а так же предоставляет функционал для удобной работы с ним.
  * В конструктор передаётся поток HTTP запроса.
- * @autor Sergey Timakov
  */
 public class HttpQuestion{
-        HttpQuestion(InputStream input) throws IOException {
-            BufferedReader message = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
-            ArrayList<String> data = new ArrayList<>();
-            HashMap<String, String> datable = new HashMap<>();
-            while (!message.ready()) ;
-            while (message.ready()) {
-                String a = message.readLine();
-                data.add(a);
-            }
 
+    private HashMap<String, String> questions = new HashMap<>();
+    private String mainQuestion;
+    HttpQuestion(InputStream input) throws IOException {
+        BufferedReader message = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
+        // ждем первой строки запроса
+        while (!message.ready()) ;
+        // считываем и печатаем все что было отправлено клиентом
+        mainQuestion=message.readLine();    //Считываем главную строку
+        while (message.ready()) {
+            String a = message.readLine();
+            String []part=a.split(":");
+
+            if(part.length>1){
+                questions.put(part[0], part[1]);
+
+            }
         }
+    }
+
+    public String getMain(){
+        return mainQuestion;
+    }
+    public String getType(){
+        String s = mainQuestion.split("/", 2)[0];
+        return s;
+    }
+    public String getProtocol(){
+        String s = mainQuestion.split("/", 3)[1];
+        return s;
+    }
+    public String getVersion(){
+        String s = mainQuestion.split("/", 3)[2];
+        return s;
+    }
 }
