8b9f2ee109e893530d676035600886ecf5149ccf
rizusss912 <71278690+rizusss912@users.noreply.github.com>
2020-09-28 00:32:04 +0300

    remove stupidity in HttpAnswer

--- src/Http/HttpAnswer.java
+++ src/Http/HttpAnswer.java
@@ -8,48 +8,43 @@ public class HttpAnswer {
     private byte[] body;
     private File file;
     private HashMap<String, String> answers = new HashMap<>();
-    private String protocol, version, code;
+    private String protocol;
+    private String version;
+    private int code;
     public byte[] getMessageToByte(){
         byte[] bytes = new byte[0];
         return bytes;
     }
 
     /**
-     * устанавливает тело функции
-     * @param body массив байтов
+     * Устанавливает тело запроса в виде массива байтов
      */
     public void setBody(byte[] body){
         this.body = body;
     }
+    /**
+     * Устанавливает тело запроса в виде файла
+     */
     public void setBody(File file){
         this.file=file;
     }
-
     /**
      * возвращает массив байтов
-     * @return массив байтов [ ]body
-     * @throws Exception если файл отсутствует или не задан
+     * @return массив байтов
+     * @throws Exception если файл/массив байтов отсутствует или не задан функцией {@link HttpAnswer#setBody(File)} {@link HttpAnswer#setBody(byte[])}
      */
     public byte[] getBody() throws Exception {
-        if (body==null){
-            if (file.exists()==true){
-                try (FileInputStream fileInput = new FileInputStream(file)){
-                    return fileInput.readAllBytes();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            else {
-                throw new Exception("File Not Found");
-            }
+        if (file.exists() == true){
+            FileInputStream fileInput = new FileInputStream(file);
+            body = fileInput.readAllBytes();
+            fileInput.close();
+        } else if (body == null) {
+            throw new Exception("on found body");
         }
-        else{
-            return body;
-        }
-        return new byte[0];
+        return body;
     }
     /**
-     * помещает элемент в мапу ответов сервера
+     * помещает элемент в HashMap ответов сервера
      * @param key заголовок
      * @param value значение
      */
@@ -58,27 +53,27 @@ public class HttpAnswer {
     }
 
     /**
-     * возвращает мапу ответов сервера
-     * @return мапа ответов сервера
+     * Возвращает HashMap ответов сервера
+     * @return HashMap ответов сервера
      */
     public HashMap getMap(){
         return answers;
     }
 
     /**
-     * возвращает значение заголовка по ключу
+     * Возвращает значение заголовка по ключу
      * @param key заголовок
      * @return значение заголовка
      */
     public String getAnswer(String key){return answers.get(key);}
 
     /**
-     * записывает протокол
-     * @param pr протокол
+     * Устанавливает тип протокол
+     * @param protocol протокол
      */
-    public void setProtocol(String pr){ this.protocol=pr;}
+    public void setProtocol(String protocol){ this.protocol=protocol;}
     /**
-     * возвращает название протокола
+     * Возвращает тип протокола
      * @return название протокола
      */
     public String getProtocol(){
@@ -86,10 +81,10 @@ public class HttpAnswer {
     }
 
     /**
-     * записывает версию протокола
+     * Устанавливает версию протокола
      * @param ver версия протокола
      */
-    public void setVersion(String ver){ this.version=ver;}
+    public void setVersion(String ver){ this.version = ver;}
     /**
      * возвращает версию протокола
      * @return весрия протокола
@@ -98,16 +93,20 @@ public class HttpAnswer {
         return version;
     }
     /**
-     * записывает код ответа сервера
+     * Устанавливает код ответа сервера
      * @param cod код ответа сервера
+     * @throws Exception переданное значение меньбше 100 или больше 599
      */
-    public void setCode(String cod){ this.code=cod;}
+    public void setCode(int cod) throws Exception {
+        if (cod >= 100 && cod < 600){
+            this.code = cod;
+        } else throw new Exception("code outside the range");
+    }
     /**
-     * возвращает код ответа
+     * Возвращает код ответа
      * @return код ответа
      */
-    public String getCode(){
+    public int getCode(){
         return code;
     }
-
 }
